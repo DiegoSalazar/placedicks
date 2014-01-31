4dc74fa52bbc4c66ca8034c80717c5c9e37750f0
Diego Salazar <salazar@kipusystems.com>
2014-01-31 13:13:15 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Placedicks::Application.config.secret_key_base = '509f527028375670277fd11e706440448af9442693f99652ea85809549e4f8c7cf82e13dd70e036e549031cbf4c1e05ac697de9fe90119dcba1f8ee0e1d9719b'
