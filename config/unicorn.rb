468de9a38393e8a9ea8410534f7506c5f7c353ee
Diego Salazar <salazar@kipusystems.com>
2014-01-31 16:10:35 -0500

    added procfile and unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,24 @@
+# config/unicorn.rb
+worker_processes 2
+timeout 30
+preload_app true
+# change backlog from standard 1024 to 200, listen to HEROKU's port environment variable
+# change it without deploy with heroku config:set UNICORN_BACKLOG=16
+listen ENV['PORT'], :backlog => Integer(ENV['UNICORN_BACKLOG'] || 200)
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to sent QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
